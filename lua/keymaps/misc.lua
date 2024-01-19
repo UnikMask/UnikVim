19f4e12cb8926fc61711ac3a98214ca5413a3015
UnikMask <alexandre.guerville@hotmail.com>
2024-01-19 18:20:30 +0100

    refactor(telescope): Formatting

--- lua/keymaps/misc.lua
+++ lua/keymaps/misc.lua
@@ -2,229 +2,238 @@ local wk = require("which-key")
 local misc = {}
 local prefix = { prefix = "<leader>" }
 
--- Neogit setup
-misc.neogit = function()
-	local neogit = function()
-		return require("neogit")
-	end
-	wk.register({
-		g = {
-			name = "Git",
-			g = {
-				function()
-					neogit().open()
-				end,
-				"Git status",
-			},
-		},
-	}, prefix)
+-- Fugitive Setup
+misc.fugitive = function()
+    local command = function(cmd)
+        return function()
+            require("lazy").load({ plugins = { "vim-fugitive" } })
+            vim.cmd(cmd)
+        end
+    end
+    wk.register({
+        g = {
+            name = "Git",
+            g = {
+                command("Git"),
+                "Toggle Git",
+            },
+            b = {
+                command("Git blame"),
+                "Toggle Git blame",
+            },
+            c = {
+                command("Git commit"),
+                "Toggle Git",
+            },
+        },
+    }, prefix)
 end
 
 -- Nvim-tree setup
 misc.nvimtree = function()
-	local api = function()
-		return require("nvim-tree.api")
-	end
-	wk.register({
-		o = {
-			name = "Open",
-			t = {
-				function()
-					api().tree.toggle()
-				end,
-				"File Explorer tree",
-			},
-		},
-	}, prefix)
+    local api = function()
+        return require("nvim-tree.api")
+    end
+    wk.register({
+        o = {
+            name = "Open",
+            t = {
+                function()
+                    api().tree.toggle()
+                end,
+                "File Explorer tree",
+            },
+        },
+    }, prefix)
 end
 
 -- Auto-session setup
 misc.auto_session = function()
-	local auto_session = function()
-		return require("auto-session")
-	end
-	wk.register({
-		q = {
-			name = "Sessions",
-			l = {
-				function()
-					auto_session().RestoreSession()
-				end,
-				"Restore last session",
-			},
-		},
-	}, prefix)
+    local auto_session = function()
+        return require("auto-session")
+    end
+    wk.register({
+        q = {
+            name = "Sessions",
+            l = {
+                function()
+                    auto_session().RestoreSession()
+                end,
+                "Restore last session",
+            },
+        },
+    }, prefix)
 end
 
 -- Lazy.nvim setup
 misc.lazy = function()
-	wk.register({
-		L = {
-			name = "Lazy",
-			l = { require("lazy").home, "Lazy Home" },
-			p = { require("lazy").profile, "Profile" },
-			h = { require("lazy").health, "Healthcheck" },
-			H = { require("lazy").help, "Help Page" },
-			d = { require("lazy").debug, "Debug Info" },
-		},
-	}, prefix)
+    wk.register({
+        L = {
+            name = "Lazy",
+            l = { require("lazy").home, "Lazy Home" },
+            p = { require("lazy").profile, "Profile" },
+            h = { require("lazy").health, "Healthcheck" },
+            H = { require("lazy").help, "Help Page" },
+            d = { require("lazy").debug, "Debug Info" },
+        },
+    }, prefix)
 end
 
 -- Orgmode setup
 misc.orgmode = function()
-	wk.register({
-		a = {
-			function()
-				require("orgmode").action("agenda.prompt", { opts = { buffer = false } })
-			end,
-			"org agenda",
-		},
-		c = {
-			function()
-				require("orgmode").action("capture.prompt", { opts = { buffer = false } })
-			end,
-			"org capture",
-		},
-	}, { prefix = "<leader>o" })
+    wk.register({
+        a = {
+            function()
+                require("orgmode").action("agenda.prompt", { opts = { buffer = false } })
+            end,
+            "org agenda",
+        },
+        c = {
+            function()
+                require("orgmode").action("capture.prompt", { opts = { buffer = false } })
+            end,
+            "org capture",
+        },
+    }, { prefix = "<leader>o" })
 end
 
 misc.notify = function()
-	wk.register({
-		n = {
-			name = "Notifications",
-			h = {
-				function()
-					vim.cmd("Notifications")
-				end,
-				"History",
-			},
-		},
-	}, prefix)
+    wk.register({
+        n = {
+            name = "Notifications",
+            h = {
+                function()
+                    vim.cmd("Notifications")
+                end,
+                "History",
+            },
+        },
+    }, prefix)
 end
 
 -- Overseer keymaps
 
 misc.overseer_init = function()
-	wk.register({
-		t = {
-			name = "Tasks",
-			o = {
-				function()
-					require("overseer").toggle()
-				end,
-				"Toggle Window",
-			},
-			t = {
-				function()
-					require("overseer").run_template()
-				end,
-				"Run Template",
-			},
-		},
-	}, prefix)
+    wk.register({
+        t = {
+            name = "Tasks",
+            o = {
+                function()
+                    require("overseer").toggle()
+                end,
+                "Toggle Window",
+            },
+            t = {
+                function()
+                    require("overseer").run_template()
+                end,
+                "Run Template",
+            },
+        },
+    }, prefix)
 end
 
 misc.overseer_config = function()
-	wk.register({
-		a = {
-			function()
-				vim.cmd("OverseerQuickAction")
-			end,
-			"Run Quick Action",
-		},
-	}, { prefix = "<leader>t" })
+    wk.register({
+        a = {
+            function()
+                vim.cmd("OverseerQuickAction")
+            end,
+            "Run Quick Action",
+        },
+    }, { prefix = "<leader>t" })
 end
 
 -- Leetbuddy keymaps
 misc.leetbuddy = function()
-	wk.register({
-		l = {
-			name = "Leetbuddy",
-			l = {
-				function()
-					local opts = require("leetbuddy").user_config
-					require("leetbuddy.questions").questions(opts)
-				end,
-				"List Questions",
-			},
-			q = {
-				function()
-					local opts = require("leetbuddy").user_config
-					require("leetbuddy.question").question(opts)
-				end,
-				"Show Question",
-			},
-			R = {
-				function()
-					local opts = require("leetbuddy").user_config
-					require("leetbuddy.reset").reset(opts)
-				end,
-				"Reset buffer",
-			},
-			s = {
-				function()
-					local opts = require("leetbuddy").user_config
-					require("leetbuddy.split").close_split(opts)
-				end,
-				"Open split",
-			},
-			k = {
-				function()
-					local opts = require("leetbuddy").user_config
-					require("leetbuddy.split").close_split(opts)
-				end,
-				"Close split",
-			},
-			t = {
-				function()
-					local opts = require("leetbuddy").user_config
-					require("leetbuddy.runner").test(opts)
-				end,
-				"Test code",
-			},
-			r = {
-				function()
-					local opts = require("leetbuddy").user_config
-					require("leetbuddy.runner").submit(opts)
-				end,
-				"Submit code",
-			},
-			A = {
-				function()
-					local opts = require("leetbuddy").user_config
-					require("leetbuddy.cookies").check_auth(opts)
-				end,
-				"Check authentication",
-			},
-		},
-	}, prefix)
+    wk.register({
+        l = {
+            name = "Leetbuddy",
+            l = {
+                function()
+                    local opts = require("leetbuddy").user_config
+                    require("leetbuddy.questions").questions(opts)
+                end,
+                "List Questions",
+            },
+            q = {
+                function()
+                    local opts = require("leetbuddy").user_config
+                    require("leetbuddy.question").question(opts)
+                end,
+                "Show Question",
+            },
+            R = {
+                function()
+                    local opts = require("leetbuddy").user_config
+                    require("leetbuddy.reset").reset(opts)
+                end,
+                "Reset buffer",
+            },
+            s = {
+                function()
+                    local opts = require("leetbuddy").user_config
+                    require("leetbuddy.split").close_split(opts)
+                end,
+                "Open split",
+            },
+            k = {
+                function()
+                    local opts = require("leetbuddy").user_config
+                    require("leetbuddy.split").close_split(opts)
+                end,
+                "Close split",
+            },
+            t = {
+                function()
+                    local opts = require("leetbuddy").user_config
+                    require("leetbuddy.runner").test(opts)
+                end,
+                "Test code",
+            },
+            r = {
+                function()
+                    local opts = require("leetbuddy").user_config
+                    require("leetbuddy.runner").submit(opts)
+                end,
+                "Submit code",
+            },
+            A = {
+                function()
+                    local opts = require("leetbuddy").user_config
+                    require("leetbuddy.cookies").check_auth(opts)
+                end,
+                "Check authentication",
+            },
+        },
+    }, prefix)
 end
 
 -- Nvim-Spider
 misc.nvimSpider = function()
-	wk.register({
-		w = {
-			function()
-				require("spider").motion("w")
-			end,
-            "Next word"
-		},
+    wk.register({
+        w = {
+            function()
+                require("spider").motion("w")
+            end,
+            "Next word",
+        },
         e = {
-            function ()
+            function()
                 require("spider").motion("e")
             end,
             "Next end of word",
         },
         b = {
-            function ()
+            function()
                 require("spider").motion("b")
             end,
             "Previous word",
         },
-	}, {
-		prefix = "",
-		mode = { "n", "v" },
-	})
+    }, {
+        prefix = "",
+        mode = { "n", "v" },
+    })
 end
 
 return misc
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,56 +1,56 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.2",
-		dependencies = {
-			"rcarriga/nvim-notify",
-			"nvim-lua/plenary.nvim",
-			"ahmedkhalf/project.nvim",
-			"rmagatti/auto-session",
-			{ "brookhong/telescope-pathogen.nvim" },
-			"tsakirist/telescope-lazy.nvim",
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-			"nvim-telescope/telescope-file-browser.nvim",
-		},
-		lazy = true,
-		cmd = { "Telescope" },
-		init = function()
-			require("keymaps.telescope")
-		end,
-		config = function()
-			-- Load Telescope
-			local telescope = require("telescope")
-			telescope.setup({
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.2",
+        dependencies = {
+            "rcarriga/nvim-notify",
+            "nvim-lua/plenary.nvim",
+            "ahmedkhalf/project.nvim",
+            "rmagatti/auto-session",
+            { "brookhong/telescope-pathogen.nvim" },
+            "tsakirist/telescope-lazy.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            "nvim-telescope/telescope-file-browser.nvim",
+        },
+        lazy = true,
+        cmd = { "Telescope" },
+        init = function()
+            require("keymaps.telescope")
+        end,
+        config = function()
+            -- Load Telescope
+            local telescope = require("telescope")
+            telescope.setup({
                 extensions = {
                     ["pathogen"] = {
                         theme = 'ivy',
                     },
                 },
-				pickers = {
-					find_files = {
-						theme = "ivy",
-					},
-				},
-			})
+                pickers = {
+                    find_files = {
+                        theme = "ivy",
+                    },
+                },
+            })
 
-			-- Setup extensions
-			require("auto-session").setup_session_lens()
+            -- Setup extensions
+            require("auto-session").setup_session_lens()
 
-			-- Load Telescope extensions
-			local load_extensions = function(extensions)
-				for _, ext in pairs(extensions) do
-					telescope.load_extension(ext)
-				end
-			end
-			load_extensions({
-				"lazy",
-				"fzf",
-				"file_browser",
-				"projects",
-				"notify",
-				"session-lens",
-				"pathogen",
-			})
-		end,
-	},
+            -- Load Telescope extensions
+            local load_extensions = function(extensions)
+                for _, ext in pairs(extensions) do
+                    telescope.load_extension(ext)
+                end
+            end
+            load_extensions({
+                "lazy",
+                "fzf",
+                "file_browser",
+                "projects",
+                "notify",
+                "session-lens",
+                "pathogen",
+            })
+        end,
+    },
 }
