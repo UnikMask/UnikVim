c716e3f8897761b58f1c88cba08e93621e996268
UnikMask <alexandre.guerville@hotmail.com>
2024-01-19 16:42:45 +0100

    feat(telescope): Search man pages and sessions

--- lua/keymaps/telescope.lua
+++ lua/keymaps/telescope.lua
@@ -3,66 +3,81 @@
 
 local wk = require("which-key")
 local bt = function()
-	return require("telescope.builtin")
+    return require("telescope.builtin")
 end
 local sfp_arg = function()
-	return {
-		cwd = vim.api.nvim_list_runtime_paths()[1],
-		hidden = true,
-	}
+    return {
+        cwd = vim.api.nvim_list_runtime_paths()[1],
+        hidden = true,
+    }
 end
 
 wk.register({
-	f = {
-		name = "Find",
-		f = {
-			function()
-				require("telescope").extensions.pathogen.find_files({hidden = true})
-			end,
-			"Files in Directory",
-		},
-		p = {
-			function()
-				bt().find_files(sfp_arg())
-			end,
-			"Files in config",
-		},
-		g = {
-			function()
-				bt().live_grep()
-			end,
-			"String in document",
-		},
-		b = {
-			function()
-				bt().buffers()
-			end,
-			"Buffers",
-		},
-		h = {
-			function()
-				bt().help_tags()
-			end,
-			"Help tags",
-		},
-		r = {
-			function()
-				bt().oldfiles()
-			end,
-			"Recent files",
-		},
-		w = {
-			function()
-				require("telescope").extensions.projects.projects({})
-			end,
-			"Project",
-		},
-		s = {
-			function()
-				require("telescope")
-				require("auto-session.session-lens").search_session()
-			end,
-			"Sessions",
-		},
-	},
+    f = {
+        name = "Find",
+        f = {
+            function()
+                require("telescope").extensions.pathogen.find_files({ hidden = true })
+            end,
+            "Files in Directory",
+        },
+        ["~"] = {
+            function()
+                require("telescope").extensions.pathogen.find_files({
+                    cwd = "~",
+                    hidden = true,
+                })
+            end,
+            "Files in Home Directory",
+        },
+        p = {
+            function()
+                bt().find_files(sfp_arg())
+            end,
+            "Files in config",
+        },
+        g = {
+            function()
+                bt().live_grep()
+            end,
+            "String in document",
+        },
+        b = {
+            function()
+                bt().buffers()
+            end,
+            "Buffers",
+        },
+        h = {
+            function()
+                bt().help_tags()
+            end,
+            "Neovim Help Tags",
+        },
+        r = {
+            function()
+                bt().oldfiles()
+            end,
+            "Recent files",
+        },
+        w = {
+            function()
+                require("telescope").extensions.projects.projects({})
+            end,
+            "Project",
+        },
+        s = {
+            function()
+                require("telescope")
+                require("auto-session.session-lens").search_session()
+            end,
+            "Sessions",
+        },
+        H = {
+            function()
+                bt().man_pages()
+            end,
+            "Man Pages",
+        }
+    },
 }, { prefix = "<leader>" })
