c39e65b9ed433392c2581fd9c0e65bd4e71bfb9e
UnikMask <alexandre.guerville@hotmail.com>
2024-02-25 12:05:57 +0100

    feat(dap): Add LLDB-debugger support

--- lua/keymaps/dap.lua
+++ lua/keymaps/dap.lua
@@ -1,123 +1,120 @@
 local wk = require("which-key")
 local dap = function()
-	return require("dap")
+    return require("dap")
 end
 local ui = function()
-	return require("dap")
+    return require("dap")
 end
 local widgets = function()
-	return require("dap.ui.widgets")
+    return require("dap.ui.widgets")
 end
 local show_widget = function(i)
-	return widgets().centered_float(i)
+    return widgets().centered_float(i)
 end
 
 return function()
-	wk.register({
-		d = {
-			name = "Debugger",
-			b = {
-				function()
-					dap().toggle_breakpoint()
-				end,
-				"Toggle breakpoint",
-			},
-			B = {
-				function()
-					dap().set_breakpoint()
-				end,
-				"Set breakpoint",
-			},
-			lp = {
-				function()
-					dap().set_breakpoint(nil, nil, vim.fn.input("Log point image: "))
-				end,
-				"Set log point",
-			},
-			j = {
-				function()
-					require("dap.ext.vscode").load_launchjs(nil, { cppdbg = { "c", "cpp" } })
-				end,
-				"Load launch.json",
-			},
-			d = {
-				name = "Run",
-				r = {
-					function()
-						dap().repl_open()
-					end,
-					"REPL",
-				},
-				l = {
-					function()
-						dap().repl_open()
-					end,
-					"Last configuration",
-				},
-				n = {
-					function()
-						dap().continue()
-					end,
-					"Run new configuration",
-				},
-			},
-			n = {
-				function()
-					dap().step_over()
-				end,
-				"Step Over",
-			},
-			p = {
-				function()
-					dap().step_back()
-				end,
-				"Step Back",
-			},
-			i = {
-				function()
-					dap().step_into()
-				end,
-				"Step Into",
-			},
-			h = {
-				function()
-					widgets().hover(widgets().frames)
-				end,
-				"Show Hover",
-			},
-			f = {
-				function()
-					show_widget(widgets().scopes)
-				end,
-				"Show Frames",
-			},
-			s = {
-				function()
-					show_widget(widgets().scopes)
-				end,
-				"Show Scopes",
-			},
+    wk.register({
+        d = {
+            name = "Debugger",
+            b = {
+                function()
+                    dap().toggle_breakpoint()
+                end,
+                "Toggle breakpoint",
+            },
+            B = {
+                function()
+                    dap().set_breakpoint()
+                end,
+                "Set breakpoint",
+            },
+            lp = {
+                function()
+                    dap().set_breakpoint(nil, nil, vim.fn.input("Log point image: "))
+                end,
+                "Set log point",
+            },
+            j = {
+                function()
+                    require("dap.ext.vscode").load_launchjs(nil, { cppdbg = { "c", "cpp" } })
+                end,
+                "Load launch.json",
+            },
+            d = {
+                name = "Run",
+                n = {
+                    function()
+                        dap().continue()
+                    end,
+                    "Run new configuration",
+                },
+                r = {
+                    function()
+                        require("dap").repl.open();
+                    end,
+                    "Open REPL",
+                }
+            },
+            n = {
+                function()
+                    dap().step_over()
+                end,
+                "Step Over",
+            },
+            p = {
+                function()
+                    dap().step_back()
+                end,
+                "Step Back",
+            },
+            i = {
+                function()
+                    dap().step_into()
+                end,
+                "Step Into",
+            },
+            w = {
+                name = "Widgets",
+                f = {
+                    function()
+                        show_widget(widgets().frames)
+                    end,
+                    "Show Frames",
+                },
+                s = {
+                    function()
+                        show_widget(widgets().scopes)
+                    end,
+                    "Show Scopes",
+                },
+                e = {
+                    function()
+                        show_widget(widgets().expressions)
+                    end,
+                    "Show Expressions",
+                },
+            },
             e = {
-                function ()
+                function()
                     ui().close()
                     dap().terminate()
                 end, "End session and close UI"
             },
             c = {
-                function ()
+                function()
                     dap().continue()
                 end, "Continue"
             },
             t = {
-                function ()
+                function()
                     dap().terminate()
                 end, "Terminate Session"
             },
             o = {
-                function ()
+                function()
                     dap().toggle()
                 end, "Toggle UI",
             },
-		},
-	}, { prefix = "<leader>" })
+        },
+    }, { prefix = "<leader>" })
 end
---
+++ lua/opts/dap.lua
@@ -0,0 +1,70 @@
+local opts = {}
+
+local find_exec = function()
+    local pickers = require("telescope.pickers")
+    local finders = require("telescope.finders")
+    local conf = require("telescope.config").values
+    local actions = require("telescope.actions")
+    local action_state = require("telescope.actions.state")
+    return coroutine.create(function(coro)
+        local telescope_opts = {}
+        pickers
+            .new(telescope_opts, {
+                prompt_title = "Path to executable",
+                finder = finders.new_oneshot_job({ "fd", "--no-ignore", "--type", "x" }, {}),
+                sorter = conf.generic_sorter(telescope_opts),
+                attach_mappings = function(buffer_number)
+                    actions.select_default:replace(function()
+                        actions.close(buffer_number)
+                        coroutine.resume(coro, action_state.get_selected_entry()[1])
+                    end)
+                    return true
+                end,
+            })
+            :find()
+    end)
+end
+
+opts.dap = function()
+    return {
+        configurations = {
+            cpp = {
+                {
+                    name = "Launch (gdb)",
+                    type = "gdb",
+                    request = "launch",
+                    program = find_exec,
+                    cwd = "${workspaceFolder}/build",
+                },
+                {
+                    name = 'Launch (lldb-vscode)',
+                    type = 'lldb',
+                    request = 'launch',
+                    program = find_exec,
+                    cwd = '${workspaceFolder}/build',
+                    stopOnEntry = false,
+                    args = {},
+                },
+            }
+        },
+        adapters = {
+            gdb = {
+                type = "executable",
+                command = "gdb",
+                args = { "-i", "dap" }
+            },
+            lldb = {
+                type = 'executable',
+                command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
+                name = 'lldb'
+            },
+        }
+    }
+end
+
+opts.dapui = {}
+opts.mason = {
+    ensure_installed = { "delve", "cppdbg" },
+}
+
+return opts
