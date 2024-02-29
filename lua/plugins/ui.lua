77cdf43228a82cb0a75bb70f5ca31640e92221de
UnikMask <alexandre.guerville@hotmail.com>
2024-02-29 22:25:57 +0100

    feat(lualine): Add modification symbols and extensions

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,57 +2,75 @@
 return {
     {
         "nvim-lualine/lualine.nvim",
-        event = 'VimEnter',
+        event = "VimEnter",
         config = function()
-            require("lualine").setup {
+            local tagbar_extension = {
+                sections = {
+                    lualine_a = { function()
+                        return "Tagbar"
+                    end }
+                },
+                filetypes = { "tagbar" },
+            }
+            require("lualine").setup({
                 options = {
                     icons_enabled = true,
-                    theme = 'auto',
-                    component_separators = '|',
-                    section_separators = { left = ' ', right = ' ' },
+                    theme = "auto",
+                    component_separators = "|",
+                    section_separators = { left = " ", right = " " },
                 },
                 sections = {
                     lualine_a = {
-                        { 'mode', separator = { left = ' ' }, right_padding = 2, }
+                        { "mode", separator = { left = " " }, right_padding = 2 },
                     },
-                    lualine_b = { 'filename', 'branch' },
-                    lualine_c = { 'fileformat' },
+                    lualine_b = {
+                        {
+                            "filename",
+                            symbols = {
+                                modified = "",
+                                readonly = "",
+                                newfile = "󰎔",
+                            },
+                        },
+                        "branch",
+                    },
+                    lualine_c = { "fileformat" },
                     lualine_x = {
                         {
-                            'diagnostics',
-                            sources = { 'nvim_diagnostic' },
-                            symbols = { error = ' ', warn = ' ', info = ' ', hint = '󰌶 ' },
+                            "diagnostics",
+                            sources = { "nvim_diagnostic", "nvim_workspace_diagnostic" },
+                            symbols = { error = " ", warn = " ", info = " ", hint = "󰌶 " },
                             colored = true,
                             update_in_insert = true,
                             always_visible = false,
                         },
                     },
-                    lualine_y = { 'filetype', 'progress' },
+                    lualine_y = { "filetype", "progress" },
                     lualine_z = {
-                        { 'location', separator = { right = ' ' }, left_padding = 2 },
+                        { "location", separator = { right = " " }, left_padding = 2 },
                     },
                 },
                 inactive_sections = {
                     lualine_a = {
-                        { 'filename', separator = { left = ' ', right = ' ' } }
+                        { "filename", separator = { left = " ", right = " " } },
                     },
                     lualine_b = {},
                     lualine_c = {},
                     lualine_x = {},
                     lualine_y = {},
                     lualine_z = {
-                        { 'location', separator = { left = ' ', right = ' ' } },
+                        { "location", separator = { left = " ", right = " " } },
                     },
                 },
                 tabline = {},
-                extensions = {},
-            }
-        end
+                extensions = { "quickfix", "overseer", "nvim-tree", "lazy", "mason", "trouble", "toggleterm", tagbar_extension },
+            })
+        end,
     },
     {
-        'stevearc/dressing.nvim',
+        "stevearc/dressing.nvim",
         lazy = true,
-        event = 'VeryLazy',
+        event = "VeryLazy",
         opts = {},
     },
 }
