9e2b5b780060fad46a0d7aabfe5ca2a9eba3df70
UnikMask <alexandre.guerville@hotmail.com>
2024-03-16 15:45:48 +0100

    feat(misc): Add Anki card plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -82,5 +82,22 @@ return {
         init = function()
             require("keymaps.misc").tagbar()
         end
+    },
+    {
+        "rareitems/anki.nvim",
+        lazy = false,
+        -- lazy -- don't lazy it, it tries to be as lazy possible and it needs to add a filetype association
+        opts = {
+            {
+                -- this function will add support for associating '.anki' extension with both 'anki' and 'tex' filetype.
+                tex_support = false,
+                models = {
+                    -- Here you specify which notetype should be associated with which deck
+                    NoteType = "PathToDeck",
+                    ["Basic"] = "Deck",
+                    ["Super Basic"] = "Deck::ChildDeck",
+                },
+            }
+        }
     }
 }
