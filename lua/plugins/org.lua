e02d7da188410909885f9bf98be587414b8a75e4
Alexandre Guerville <alexandre.guerville@techex.co.uk>
2024-09-30 10:38:42 +0100

    feat(org): Update org config

--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -1,12 +1,11 @@
 return {
     {
-        'nvim-orgmode/orgmode',
+        "nvim-orgmode/orgmode",
         lazy = true,
         dependencies = {
-            'nvim-treesitter/nvim-treesitter',
-            'hrsh7th/nvim-cmp',
+            "hrsh7th/nvim-cmp",
             {
-                'akinsho/org-bullets.nvim',
+                "akinsho/org-bullets.nvim",
                 opts = {
                     list = "•",
                     headlines = { "◉", "○", "✸", "✿" },
@@ -18,18 +17,18 @@ return {
                 },
             },
         },
-        ft = { 'org' },
+        ft = { "org" },
         opts = {
-            org_agenda_files = { '~/org/agenda/*' },
-            org_default_notes_file = '~/org/agenda/notes.org',
-            emacs_config = { executable_path = '/usr/bin/emacs', config_path = '$HOME/.emacs.d/early-init.el' },
+            org_agenda_files = { "~/org/**/*.org" },
+            org_default_notes_file = "~/org/notes.org",
+            emacs_config = { executable_path = "/usr/bin/emacs", config_path = "~/.config/emacs/early-init.el" },
             org_custom_exports = {
                 d = {
-                    label = 'Doom Export to PDF',
+                    label = "Doom Export to PDF",
                     action = function(exporter)
                         local current_file = vim.api.nvim_buf_get_name(0)
-                        local target = vim.fn.fnamemodify(current_file, ':p:r') .. '.pdf'
-                        local cmd = string.format('export-notes', current_file)
+                        local target = vim.fn.fnamemodify(current_file, ":p:r") .. ".pdf"
+                        local cmd = string.format("export-notes", current_file)
                         local on_error = function(err)
                             vim.api.nvim_echo(err, true, {})
                         end
@@ -37,41 +36,51 @@ return {
                     end,
                 },
             },
-            org_capture_templates = {
-                j = {
-                    description = 'Journal',
-                    template = '\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?',
-                    target = '~/org/agenda/journal.org'
-                },
-                T = {
-                    description = 'Todo',
-                    template = '* TODO %?\n %u',
-                    target = '~/org/agenda/todo.org'
-                },
-            },
+            -- org_capture_templates = {
+            --     j = {
+            --         description = 'Journal',
+            --         template = '\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?',
+            --         target = '~/org/agenda/journal.org'
+            --     },
+            --     T = {
+            --         description = 'Todo',
+            --         template = '* TODO %?\n %u',
+            --         target = '~/org/agenda/todo.org'
+            --     },
+            -- },
             notifications = {
                 reminder_time = { 0, 5, 10 },
             },
         },
         init = function()
-            require('keymaps.misc').orgmode()
+            require("keymaps.misc").orgmode()
         end,
         config = function(_, opts)
-            require('orgmode').setup_ts_grammar()
-            require('nvim-treesitter.configs').setup({
+            require("nvim-treesitter.configs").setup({
                 highlight = {
                     enable = true,
-                    additional_vim_regex_highlighting = { 'org' },
+                    additional_vim_regex_highlighting = { "org" },
                 },
-                ensure_installed = { 'org' },
+                ensure_installed = { "org" },
             })
-            require('orgmode').setup(opts)
-            require('cmp').setup.filetype('org', {
-                sources = require('cmp').config.sources({
-                    { name = 'orgmode' },
-                }, { name = 'buffer' })
+            require("orgmode").setup(opts)
+            require("cmp").setup.filetype("org", {
+                sources = require("cmp").config.sources({
+                    { name = "orgmode" },
+                }, { name = "buffer" }),
             })
-        end
-    }
-
+        end,
+    },
+    {
+        "chipsenkbeil/org-roam.nvim",
+        lazy = true,
+        event = "VeryLazy",
+        ft = { "org" },
+        dependencies = {
+            "nvim-orgmode/orgmode",
+        },
+        config = function()
+            require("org-roam").setup(require("opts.org").roam())
+        end,
+    },
 }
