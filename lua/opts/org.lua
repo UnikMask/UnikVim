5edd57da71a2ebddf0c10e87717946be778c48ec
Alexandre Guerville <alexandre.guerville@techex.co.uk>
2025-03-07 16:39:14 +0000

    refactor(org): Move orgmode config to own opts file & use stable version

--- lua/opts/org.lua
+++ lua/opts/org.lua
@@ -6,4 +6,65 @@ M.roam = function()
     }
 end
 
+M.org = function()
+    return {
+        org_agenda_files = { "~/org/**/*.org" },
+        org_default_notes_file = "~/org/notes.org",
+        emacs_config = { executable_path = "/usr/bin/emacs", config_path = "~/.config/emacs/early-init.el" },
+        org_custom_exports = {
+            d = {
+                label = "Doom Export to PDF",
+                action = function(exporter)
+                    local current_file = vim.api.nvim_buf_get_name(0)
+                    local target = vim.fn.fnamemodify(current_file, ":p:r") .. ".pdf"
+                    local cmd = string.format("export-notes", current_file)
+                    local on_error = function(err)
+                        vim.api.nvim_echo(err, true, {})
+                    end
+                    return exporter({ cmd }, target, nil, on_error)
+                end,
+            },
+        },
+        org_capture_templates = {
+            s = {
+                description = 'Standup Summary',
+                template = '* %u Standup Summary\n\n',
+                target = '~/org/standups/standup-%<%Y-%m-%d>.org'
+            },
+            T = {
+                description = 'Todo',
+                template = '* TODO %?\n %u',
+                target = '~/org/todo.org'
+            },
+            j = {
+                description = 'Journal',
+                template = '\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?',
+                target = '~/org/journal.org'
+            },
+            J = {
+                description = 'Journal',
+                template = '\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?',
+                target = '~/sync/org/journal/%<%Y-%m>.org'
+            },
+            e = {
+                description = 'Event',
+                subtemplates = {
+                    r = {
+                        description = 'recurring',
+                        template = '** %?\n %T',
+                        target = '~/org/calendar.org',
+                        headline = 'recurring'
+                    },
+                    o = {
+                        description = 'one-time',
+                        template = '** %?\n %T',
+                        target = '~/org/calendar.org',
+                        headline = 'one-time'
+                    },
+                },
+            },
+        }
+    }
+end
+
 return M
--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -1,6 +1,7 @@
 return {
     {
         "nvim-orgmode/orgmode",
+        version = "*",
         lazy = true,
         dependencies = {
             "hrsh7th/nvim-cmp",
@@ -18,40 +19,6 @@ return {
             },
         },
         ft = { "org" },
-        opts = {
-            org_agenda_files = { "~/org/**/*.org" },
-            org_default_notes_file = "~/org/notes.org",
-            emacs_config = { executable_path = "/usr/bin/emacs", config_path = "~/.config/emacs/early-init.el" },
-            org_custom_exports = {
-                d = {
-                    label = "Doom Export to PDF",
-                    action = function(exporter)
-                        local current_file = vim.api.nvim_buf_get_name(0)
-                        local target = vim.fn.fnamemodify(current_file, ":p:r") .. ".pdf"
-                        local cmd = string.format("export-notes", current_file)
-                        local on_error = function(err)
-                            vim.api.nvim_echo(err, true, {})
-                        end
-                        return exporter({ cmd }, target, nil, on_error)
-                    end,
-                },
-            },
-            -- org_capture_templates = {
-            --     j = {
-            --         description = 'Journal',
-            --         template = '\n*** %<%Y-%m-%d> %<%A>\n**** %U\n\n%?',
-            --         target = '~/org/agenda/journal.org'
-            --     },
-            --     T = {
-            --         description = 'Todo',
-            --         template = '* TODO %?\n %u',
-            --         target = '~/org/agenda/todo.org'
-            --     },
-            -- },
-            notifications = {
-                reminder_time = { 0, 5, 10 },
-            },
-        },
         init = function()
             require("keymaps.misc").orgmode()
         end,
@@ -63,7 +30,7 @@ return {
                 },
                 ensure_installed = { "org" },
             })
-            require("orgmode").setup(opts)
+            require("orgmode").setup(require("opts.org").org())
             require("cmp").setup.filetype("org", {
                 sources = require("cmp").config.sources({
                     { name = "orgmode" },
