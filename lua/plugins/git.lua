c5329e1fc03fbe9a38c010699fb323e222b4eebc
Alexandre Guerville <alexandre.guerville@techex.co.uk>
2025-06-01 00:38:34 +0100

    fix(git): Force neogit to non-breaking version

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,35 +1,36 @@
 return {
-    {
-        'NeogitOrg/neogit',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope.nvim',
-            'sindrets/diffview.nvim',
-        },
-        lazy = true,
-        cmd = { 'Neogit' },
-        keys = require("keymaps.misc").neogit,
-        opts = {
-            kind = 'split_above',
-            popup = {
-                kind = 'split_above',
-            },
-            disable_builtin_notifications = true,
-            integrations = {
-                telescope = true,
-            }
-        },
-    },
-    {
-        "lewis6991/gitsigns.nvim",
-        lazy = true,
-        event = "BufRead",
-        keys = require("keymaps.misc").gitsigns,
-        opts = {
-            signcolumn = true,
-            current_line_blame_opts = {
-                delay = 50,
-            }
-        },
-    },
+	{
+		"NeogitOrg/neogit",
+		version = "2.0.0",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"sindrets/diffview.nvim",
+		},
+		lazy = true,
+		cmd = { "Neogit" },
+		keys = require("keymaps.misc").neogit,
+		opts = {
+			kind = "split_above",
+			popup = {
+				kind = "split_above",
+			},
+			disable_builtin_notifications = true,
+			integrations = {
+				telescope = true,
+			},
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		lazy = true,
+		event = "BufRead",
+		keys = require("keymaps.misc").gitsigns,
+		opts = {
+			signcolumn = true,
+			current_line_blame_opts = {
+				delay = 50,
+			},
+		},
+	},
 }
